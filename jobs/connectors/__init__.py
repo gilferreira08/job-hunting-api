8819a9b89596d46e816a2c9e44cba6e0ca54d5a0
gilferreira08 <gilferreira08@yahoo.com.br>
2026-05-11 12:35:26 +0200

    Export agreed four-source connectors

--- jobs/connectors/__init__.py
+++ jobs/connectors/__init__.py
@@ -1,13 +1,13 @@
 from jobs.connectors.base_connector import BaseConnector
-from jobs.connectors.company_careers_connector import CompanyCareersConnector
 from jobs.connectors.efinancialcareers_connector import EFinancialCareersConnector
-from jobs.connectors.france_travail_connector import FranceTravailConnector
+from jobs.connectors.hellowork_connector import HelloWorkConnector
+from jobs.connectors.indeed_connector import IndeedConnector
 from jobs.connectors.wttj_connector import WTTJConnector
 
 __all__ = [
     "BaseConnector",
-    "CompanyCareersConnector",
     "EFinancialCareersConnector",
-    "FranceTravailConnector",
+    "HelloWorkConnector",
+    "IndeedConnector",
     "WTTJConnector",
 ]
